3b1834112a47019eea2f70e8c55c35af4a515f45
Tanner555 <theman989898@gmail.com>
2016-03-11 07:25:07 -0800

    Initial Commit
    This is my first commit while I'm following the s3 fps tutorial. I'll be
    using this in the future for prototyping.

---
+++ FPS Prototype/Assets/FPS Prototyping/my scripts/Master Scripts/GameManager_Master.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace S3
+{
+    public class GameManager_Master : MonoBehaviour
+    {
+        public delegate void GameManagerEventHandler();
+        public event GameManagerEventHandler MenuToggleEvent;
+        public event GameManagerEventHandler InventoryUIToggleEvent;
+        public event GameManagerEventHandler RestartToggleEvent;
+        public event GameManagerEventHandler GoToMenuSceneEvent;
+        public event GameManagerEventHandler GameOverEvent;
+
+        public bool isGameOver;
+        public bool isInventoryUIOn;
+        public bool isMenuOn;
+
+        public void CallEventMenuToggle()
+        {
+            if(MenuToggleEvent != null)
+            {
+                MenuToggleEvent();
+            }
+        }
+
+        public void CallInventoryUIToggle()
+        {
+            if(InventoryUIToggleEvent != null)
+            {
+                InventoryUIToggleEvent();
+            }
+        }
+
+        public void CallEventRestartLevel()
+        {
+            if (RestartToggleEvent != null)
+            {
+                RestartToggleEvent();
+            }
+        }
+
+        public void CallEventMenuScene()
+        {
+            if (GoToMenuSceneEvent != null)
+            {
+                GoToMenuSceneEvent();
+            }
+        }
+
+        public void CallEventGameOver()
+        {
+            if (GameOverEvent != null)
+            {
+                GameOverEvent();
+            }
+        }
+    }
+}
