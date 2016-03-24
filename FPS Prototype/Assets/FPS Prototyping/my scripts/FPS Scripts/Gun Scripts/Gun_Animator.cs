5b52ab599303d7fd18e4798185982b2f2027e5ce
Tanner555 <theman989898@gmail.com>
2016-03-24 02:25:51 -0700

    Completed the Gun Framework
    I've completed chapter 6 of the tutorial.

---
+++ FPS Prototype/Assets/FPS Prototyping/my scripts/FPS Scripts/Gun Scripts/Gun_Animator.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace S3
+{
+    public class Gun_Animator : MonoBehaviour
+    {
+        private Gun_Master gunMaster;
+        private Animator myAnimator;
+
+        void OnEnable()
+        {
+            SetInitialReferences();
+            gunMaster.EventPlayerInput += PlayShootAnimation;
+        }
+        void OnDisable()
+        {
+
+        }
+        void SetInitialReferences()
+        {
+            gunMaster = GetComponent<Gun_Master>();
+            if(GetComponent<Animator>() != null)
+            {
+                myAnimator = GetComponent<Animator>();
+            }
+        }
+
+        void PlayShootAnimation()
+        {
+            if(myAnimator != null)
+            {
+                myAnimator.SetTrigger("Shoot");
+            }
+        }
+
+    }
+}
